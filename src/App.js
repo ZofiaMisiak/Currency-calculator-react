0505acf76d55bf0ba4c1ef4b141ecea9942bd597
Zofia Misiak <zosia.misiak@gmail.com>
2023-01-09 21:47:36 +0200

    img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 function App() {
   return (
-    <div> Dzień dobry!
-    </div>
+    
+    <div style={{ backgroundImage: "url(public/img/tlo.png)" }}>
+    Hello World
+  </div>
   );
 }
 
