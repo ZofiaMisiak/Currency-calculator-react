81e17341b2dc9012a378213c97cf4f658e48916c
Zofia Misiak <zosia.misiak@gmail.com>
2023-01-19 15:14:51 +0200

    point

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -11,12 +11,13 @@ const formatDate = (date) => date.toLocaleString(undefined, {
 });
 
 export const Clock = () => {
+    
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval()) => {
          setDate(new Date());
-    } 1000;
+    }, 1000);
 
     return () => {
         clearInterval(intervalId);
