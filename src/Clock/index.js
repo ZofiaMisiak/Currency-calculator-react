9acd0089f7b3a85b2e4e0bf2a016e448a8e4b611
Zofia Misiak <zosia.misiak@gmail.com>
2023-01-19 15:18:32 +0200

    wew

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -15,7 +15,7 @@ export const Clock = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        const intervalId = setInterval()) => {
+        const intervalId = setInterval() => {
          setDate(new Date());
     }, 1000);
 
